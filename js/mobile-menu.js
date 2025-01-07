19a937e95ee72bc9e7be292603f44cdbdef05764
Vladislav <vlad11ponomarev@gmail.com>
2025-01-07 23:29:38 +0200

    add desktop styles

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -15,19 +15,19 @@
 })();
 
 const btnUp = {
-  el: document.querySelector('.btn-up'),
+  el: document.querySelector('.btn'),
   show() {
-    this.el.classList.remove('btn-up_hide');
+    this.el.classList.remove('btn-hide');
   },
   hide() {
-    this.el.classList.add('btn-up_hide');
+    this.el.classList.add('btn-hide');
   },
   addEventListener() {
     window.addEventListener('scroll', () => {
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       scrollY > 400 ? this.show() : this.hide();
     });
-    document.querySelector('.btn-up').onclick = () => {
+    document.querySelector('.btn').onclick = () => {
       window.scrollTo({
         top: 0,
         left: 0,
--- js/numbers.js
+++ js/numbers.js
@@ -1,27 +1,3 @@
-// function animateNumbers() {
-//             const counters = document.querySelectorAll('.number');
-
-//             counters.forEach(counter => {
-//                 const target = +counter.getAttribute('data-target');
-//                 let current = 0;
-//                 const increment = target / 100;
-
-//                 const updateCounter = () => {
-//                     current += increment;
-//                     if (current < target) {
-//                         counter.textContent = `${Math.ceil(current)}+`;
-//                         requestAnimationFrame(updateCounter);
-//                     } else {
-//                         counter.textContent = `${target}+`;
-//                     }
-//                 };
-
-//                 updateCounter();
-//             });
-//         }
-
-//         window.addEventListener('load', animateNumbers);
-
  function animateNumbers() {
             const counters = document.querySelectorAll('.number');
 
